aa31c0d36dc5e94959efba71597f8be0a920ecb1
Thomas <tranthomas494@gmail.com>
2026-02-23 13:30:56 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/rocket_launch/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rocket_launch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rocket Launch Controller',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CounterWidget(),
+    );
+  }
+}
+
+class CounterWidget extends StatefulWidget {
+  @override
+  _CounterWidgetState createState() => _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  // set counter value
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Rocket Launch Controller'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Container(
+              color: Colors.blue,
+              child: Text(
+                '$_counter',
+                style: const TextStyle(fontSize: 50.0),
+              ),
+            ),
+          ),
+          Slider(
+            min: 0,
+            max: 100,
+            value: _counter.toDouble(),
+            onChanged: (double value) {
+              setState(() {
+                _counter = value.toInt();
+              });
+            },
+            activeColor: Colors.blue,
+            inactiveColor: Colors.red,
+          ),
+        ],
+      ),
+    );
+  }
+}
